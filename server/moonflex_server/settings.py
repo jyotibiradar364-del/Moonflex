bd281cee09f1bc2f9fff9c726bd24ec62cf7726b
Jyoti Biradar <jyotibiradar364@gmail.com>
2026-03-06 11:30:35 +0530

    fix: hardcode ALLOWED_HOSTS for render deployment

--- server/moonflex_server/settings.py
+++ server/moonflex_server/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,.onrender.com').split(',')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
 
 # Application definition
